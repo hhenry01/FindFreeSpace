2ebf797c22258926680086ddff2272be6a5d66b5
Henry Huang <henryhuang2001@gmail.com>
2021-01-10 02:48:13 -0800

    Email notifications work as intended.

--- notification.py
+++ notification.py
@@ -1,6 +1,11 @@
 import pymongo
 from pymongo import MongoClient
-import smtplib, ssl, numpy
+import smtplib, ssl, numpy, email
+
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 cluster = MongoClient("mongodb+srv://pogchamp:poggers@findfreespace-web.34zzj.mongodb.net/findfreespace?retryWrites=true&w=majority")
 db = cluster["findfreespace"]
@@ -13,10 +18,19 @@ def sendEmail(message, receiver_email):
     password = "findfree.space.nwhacks2021"
     smtp_server = "smtp.gmail.com"
     port = 465
+
+    finalMessage = MIMEMultipart()
+    finalMessage["From"] = sender_email
+    finalMessage["To"] = receiver_email
+    finalMessage["Subject"] = "Study rooms available!"
+    finalMessage.attach(MIMEText(message, "plain"))
+
+    text = finalMessage.as_string()
+
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context = context) as server: 
         server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
+        server.sendmail(sender_email, receiver_email, text)
 
 while True:
     listEmptyRooms = []
@@ -29,22 +43,15 @@ while True:
         if person["searching"] == True:
             validEmptyRoomCameras = []
             for camera in listEmptyRooms:
-                if camera in person["requestedRoomIDs"]:
+                if str(camera["_id"]) in person["requestedRoomIDs"]:
                     validEmptyRoomCameras.append(camera)
             emailQueue.update({person["_id"]: tuple(validEmptyRoomCameras)})
 
     for person in emailQueue.keys():
         emptyRooms = emailQueue[person] # list containing all cameras requested by client
-        message = """\
-            Subject: Empty rooms available!
-            
-
-            The following study rooms are available:"""
-        messageAddendum = ""
+        message = "The following study rooms are available:"
         for camera in emptyRooms:
-            messageAddendum += "\n" + camera["building"] + "-" + camera["room_num"]
+            message += "\n" + str(camera["building"]) + "-" + str(camera["room_num"])
         currentClient = notifications.find_one({"_id": person})
-        sendEmail(message + messageAddendum, currentClient["email"])
-        break #for testing
+        sendEmail(message, currentClient["email"])
         notifications.update_one({"_id": currentClient["_id"]}, {"$set": {"searching": False}})
-    break #testing
