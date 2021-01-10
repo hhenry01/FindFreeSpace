3f6e5a745185e30397293618c31c1ce29914c321
Henry Huang <henryhuang2001@gmail.com>
2021-01-09 17:58:02 -0800

    Trying to use the mode over 3 seconds to determine #people

--- facetracking.py
+++ facetracking.py
@@ -1,40 +1,53 @@
 import cv2
 import numpy
+import time
+import statistics
 
 # Load the cascade
-face_cascade = cv2.CascadeClassifier('haarcascade_upperbody.xml')
+cascade = cv2.CascadeClassifier('haarcascade_upperbody.xml')
 
-# To capture video from webcam. 
+# Get video from default device
 cap = cv2.VideoCapture(0)
-# To use a video file as input 
-# cap = cv2.VideoCapture('filename.mp4')
 
-while True:
+# Prep processing
+startInterval = time.time()
+countList = []
+
+
+while (time.time() - 3 < startInterval):
     # Read the frame
     _, img = cap.read()
 
     # Convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Detect the faces
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # Detect 
+    people = cascade.detectMultiScale(gray, 1.1, 4) #color, scalefactor, min neighbors
+
+    # Process 
     num = 0
-    if type(faces) is numpy.ndarray:
-        for x in faces:
+    if type(people) is numpy.ndarray:
+        for x in people:
             num = num + 1
+    countList.append(num)
     print(num)
 
     # Draw the rectangle around each face for testing
-    for (x, y, w, h) in faces:
+    for (x, y, w, h) in people:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
     # Display
     cv2.imshow('img', img)
 
     # Stop if escape key is pressed
-    k = cv2.waitKey(30) & 0xff
-    if k==27:
+    if cv2.waitKey(1) == 27:
         break
+
+result = statistics.mode(countList)
+print(result)
     
 # Release the VideoCapture object
 cap.release()
+
+#Destroy window showing video
+cv2.destroyAllWindows()
