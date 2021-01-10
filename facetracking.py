1c950c6f79633f939f825dca40d3e3f18ecc1e4c
Henry Huang <henryhuang2001@gmail.com>
2021-01-09 19:20:08 -0800

    Trying to get database connection working

--- facetracking.py
+++ facetracking.py
@@ -2,6 +2,12 @@ import cv2
 import numpy
 import time
 import statistics
+import pymongo
+from pymongo import MongoClient
+
+cluster = MongoClient("mongodb+srv://pogchamp:poggers@findfreespace-web.34zzj.mongodb.net/findfreespace?retryWrites=true&w=majority")
+db = cluster["findfreespace"]
+collection = db["cameras"]
 
 # Load the cascade
 cascade = cv2.CascadeClassifier('haarcascade_upperbody.xml')
@@ -9,41 +15,54 @@ cascade = cv2.CascadeClassifier('haarcascade_upperbody.xml')
 # Get video from default device
 cap = cv2.VideoCapture(0)
 
-# Prep processing
-startInterval = time.time()
-countList = []
+id = 101
+room_num = 101
+stop = False
 
+collection.insert_one({"_id": id, "room_num": room_num})
 
-while (time.time() - 3 < startInterval):
-    # Read the frame
-    _, img = cap.read()
+while true:
+    # Prep processing
+    startInterval = time.time()
+    countList = []
+    
+    while (time.time() - 30 < startInterval):
+        # Read the frame
+        _, img = cap.read()
 
-    # Convert to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Convert to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Detect 
-    people = cascade.detectMultiScale(gray, 1.1, 4) #color, scalefactor, min neighbors
+        # Detect 
+        people = cascade.detectMultiScale(gray, 1.1, 4) #color, scalefactor, min neighbors
 
-    # Process 
-    num = 0
-    if type(people) is numpy.ndarray:
-        for x in people:
-            num = num + 1
-    countList.append(num)
-    print(num)
+        # Process 
+        num = 0
+        if type(people) is numpy.ndarray:
+            for x in people:
+                num = num + 1
+        countList.append(num)
 
-    # Draw the rectangle around each face for testing
-    for (x, y, w, h) in people:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        # Draw the rectangle around each face for testing
+        for (x, y, w, h) in people:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-    # Display
-    cv2.imshow('img', img)
+        # Display
+        cv2.imshow('img', img)
 
-    # Stop if escape key is pressed
-    if cv2.waitKey(1) == 27:
+        # Stop if escape key is pressed
+        if cv2.waitKey(1) == 27:
+            break
+            stop = True
+
+    result = statistics.mode(countList)
+    
+
+    collection.update_one({"_id": id}, {"$set": {"num_people": result}})
+
+    if stop or cv2.waitKey(1) == 27: 
         break
-
-result = statistics.mode(countList)
+    
 print(result)
     
 # Release the VideoCapture object
