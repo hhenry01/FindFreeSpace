a240c99fd9b1f12d8ea7176c14449a35012d00a5
Henry Huang <henryhuang2001@gmail.com>
2021-01-10 03:18:26 -0800

    Main bug: emails sent twice

--- notification.py
+++ notification.py
@@ -1,11 +1,13 @@
 import pymongo
 from pymongo import MongoClient
-import smtplib, ssl, numpy, email
+import smtplib, ssl, numpy, email, time
 
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+
+NOTIFICATION_COOLDOWN = 10 #min number of seconds between email notifications. Right now 10s for testing
 
 cluster = MongoClient("mongodb+srv://pogchamp:poggers@findfreespace-web.34zzj.mongodb.net/findfreespace?retryWrites=true&w=majority")
 db = cluster["findfreespace"]
@@ -41,17 +43,26 @@ while True:
     emailQueue = {}
     for person in notifications.find({}):
         if person["searching"] == True:
+            print(time.time())
             validEmptyRoomCameras = []
             for camera in listEmptyRooms:
                 if str(camera["_id"]) in person["requestedRoomIDs"]:
                     validEmptyRoomCameras.append(camera)
             emailQueue.update({person["_id"]: tuple(validEmptyRoomCameras)})
+        else:
+            if person["cooldown"] <= time.time():   # check to see if timeout is done
+                notifications.update_one({"_id": person["_id"]}, {"$set": {"searching": True}})
 
     for person in emailQueue.keys():
         emptyRooms = emailQueue[person] # list containing all cameras requested by client
         message = "The following study rooms are available:"
         for camera in emptyRooms:
-            message += "\n" + str(camera["building"]) + "-" + str(camera["room_num"])
+            building = camera["building"]
+            building = building.capitalize()
+            message += "\n" + building + "-" + str(camera["room_num"])
         currentClient = notifications.find_one({"_id": person})
         sendEmail(message, currentClient["email"])
         notifications.update_one({"_id": currentClient["_id"]}, {"$set": {"searching": False}})
+        cooldown = time.time() + NOTIFICATION_COOLDOWN
+        notifications.update_one({"_id": currentClient["_id"]}, {"$set": {"cooldown": cooldown}})
+        ## set user timeout to specified time, e.g. 30 min or 1 hour - "$set": {"cooldown": time.time() + 1800}
