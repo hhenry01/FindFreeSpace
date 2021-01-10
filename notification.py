903e4b168057b692a4fa6a6787d1ddb2536f68a4
Henry Huang <henryhuang2001@gmail.com>
2021-01-09 21:02:00 -0800

    Emails are sending

--- notification.py
+++ notification.py
@@ -2,15 +2,23 @@ import pymongo
 from pymongo import MongoClient
 import smtplib, ssl
 
+smtp_server = "smtp.gmail.com"
 port = 465
+sender_email = "FFSnwhacks@gmail.com"
+receiver_email = "FFSnwhacks@gmail.com"
 password = "findfree.space.nwhacks2021"
+message = """\
+    Subject: Hi there
 
-context = ssl.create_default_context
+    This message is sent from Python."""
 
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context = context) as server: 
-    server.login("FFSnwhacks@gmail.com", password)
+context = ssl.create_default_context()
 
 cluster = MongoClient("mongodb+srv://pogchamp:poggers@findfreespace-web.34zzj.mongodb.net/findfreespace?retryWrites=true&w=majority")
 db = cluster["findfreespace"]
 cameras = db["cameras"]
 buildings = db["buildings"]
+
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context = context) as server: 
+    server.login("FFSnwhacks@gmail.com", password)
+    server.sendmail(sender_email, receiver_email, message)
